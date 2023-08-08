1d3720069aa7c36b5a0dafaf88dda79b1b50c69d
IlyaKhokhanov <hohanov.ilya@gmail.com>
2023-08-08 18:11:45 +0300

    feat: add second address and log in button

--- src/pages/registration/Registration.tsx
+++ src/pages/registration/Registration.tsx
@@ -1,5 +1,14 @@
-import React from 'react';
-import { Button, DatePicker, Form, Input, Select } from 'antd';
+import React, { useState } from 'react';
+import {
+  Button,
+  DatePicker,
+  Form,
+  Input,
+  Select,
+  Checkbox,
+  Row,
+  Col,
+} from 'antd';
 
 const { Option } = Select;
 
@@ -27,10 +36,28 @@ const tailFormItemLayout = {
   },
 };
 
+interface IRegistrationForm {
+  email: string;
+  firstName: string;
+  lastName: string;
+  password: string;
+  confirm: string;
+  birthday: object;
+  city: string;
+  country: string;
+  postalCode: string;
+  street: string;
+  cityBilling?: string;
+  countryBilling?: string;
+  postalCodeBilling?: string;
+  streetBilling?: string;
+}
+
 function Registration(): JSX.Element {
   const [form] = Form.useForm();
+  const [oneAdress, setOneAdress] = useState(true);
 
-  const onFinish = (values: any) => {
+  const onFinish = (values: IRegistrationForm) => {
     console.log('Received values of form: ', values);
   };
 
@@ -40,16 +67,19 @@ function Registration(): JSX.Element {
       form={form}
       name="register"
       onFinish={onFinish}
-      initialValues={{
-        residence: ['zhejiang', 'hangzhou', 'xihu'],
-        prefix: '86',
+      style={{
+        margin: '0 auto',
+        maxWidth: 600,
+        padding: '10px 50px 10px 0',
+        border: 'solid 1px #f0f0f0',
+        borderRadius: '.5em',
       }}
-      style={{ maxWidth: 600 }}
       scrollToFirstError
     >
       <Form.Item {...tailFormItemLayout}>
         <h1>Create Account</h1>
       </Form.Item>
+
       <Form.Item
         name="email"
         label="E-mail"
@@ -65,7 +95,7 @@ function Registration(): JSX.Element {
           },
         ]}
       >
-        <Input placeholder="E-mail" />
+        <Input placeholder="E-mail" value="hi" />
       </Form.Item>
 
       <Form.Item
@@ -73,9 +103,12 @@ function Registration(): JSX.Element {
         label="Password"
         hasFeedback
         rules={[
+          { required: true, message: 'Please input your password' },
+          { min: 8, message: 'Password must be at least 8 characters long' },
           {
-            required: true,
-            message: 'Please input your password!',
+            pattern: /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[!@#$%^&*])/,
+            message:
+              'Password must contain at least one lowercase letter, one uppercase letter, one digit and one special character',
           },
         ]}
       >
@@ -153,6 +186,25 @@ function Registration(): JSX.Element {
       </Form.Item>
 
       <Form.Item
+        name="remember"
+        valuePropName="checked"
+        wrapperCol={{ offset: 8, span: 16 }}
+      >
+        <Checkbox
+          defaultChecked={oneAdress}
+          onChange={() => setOneAdress(!oneAdress)}
+        >
+          Use the same address for both billing and shipping
+        </Checkbox>
+      </Form.Item>
+
+      {!oneAdress && (
+        <Form.Item {...tailFormItemLayout}>
+          <h3>Address for shipping</h3>
+        </Form.Item>
+      )}
+
+      <Form.Item
         name="country"
         label="Country"
         hasFeedback
@@ -210,10 +262,82 @@ function Registration(): JSX.Element {
         <Input placeholder="Street" />
       </Form.Item>
 
+      {!oneAdress && (
+        <>
+          <Form.Item {...tailFormItemLayout}>
+            <h3>Address for billing</h3>
+          </Form.Item>
+
+          <Form.Item
+            name="countryBilling"
+            label="Country"
+            hasFeedback
+            rules={[{ required: true, message: 'Please select your country!' }]}
+          >
+            <Select placeholder="Please select a country">
+              <Option value="usa">U.S.A</Option>
+              <Option value="russia">Russia</Option>
+            </Select>
+          </Form.Item>
+          <Form.Item
+            name="postalCodeBilling"
+            label="Postal code"
+            hasFeedback
+            dependencies={['countryBilling']}
+            rules={[
+              {
+                required: true,
+                message: 'Please input postal code',
+                whitespace: true,
+              },
+            ]}
+          >
+            <Input placeholder="Postal code" />
+          </Form.Item>
+          <Form.Item
+            name="cityBilling"
+            label="City"
+            hasFeedback
+            rules={[
+              {
+                required: true,
+                message: 'Please input city',
+                whitespace: true,
+              },
+            ]}
+          >
+            <Input placeholder="City" />
+          </Form.Item>
+          <Form.Item
+            name="streetBilling"
+            label="Street"
+            hasFeedback
+            rules={[
+              {
+                required: true,
+                message: 'Please input street',
+                whitespace: true,
+              },
+            ]}
+          >
+            <Input placeholder="Street" />
+          </Form.Item>
+        </>
+      )}
+
       <Form.Item {...tailFormItemLayout}>
-        <Button type="primary" htmlType="submit">
-          Register
-        </Button>
+        <Row gutter={16}>
+          <Col span={6}>
+            <Button type="primary" htmlType="submit">
+              Register
+            </Button>
+          </Col>
+          <Col span={6}>
+            <Button type="primary" htmlType="submit">
+              Log in
+            </Button>
+          </Col>
+        </Row>
       </Form.Item>
     </Form>
   );
