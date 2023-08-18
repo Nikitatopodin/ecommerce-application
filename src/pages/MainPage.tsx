c2e8f3fa5aff5a2e60a380e3de189079763ed5c8
Nikitatopodin <Nikitatop1@gmail.com>
2023-08-18 10:54:22 +0300

    feat: improve routing

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,16 +1,12 @@
 import React from 'react';
 import { Layout, Space } from 'antd';
-import HeaderComponent from '../layouts/header/header';
-import FooterComponent from '../layouts/footer/footer';
-import MainComponent from '../layouts/main/main';
+import MainComponent from '../layouts/main/Main';
 
 function Main(): JSX.Element {
   return (
     <Space direction="vertical" style={{ width: '100%' }} size={[0, 48]}>
       <Layout>
-        <HeaderComponent />
         <MainComponent />
-        <FooterComponent />
       </Layout>
     </Space>
   );
