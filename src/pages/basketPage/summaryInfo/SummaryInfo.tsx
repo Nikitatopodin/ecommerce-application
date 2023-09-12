fc205c8acc414cc361d458fc859573b0e0da985b
Maria Bogdanova <masha.bogdanoff@gmail.com>
2023-09-12 12:34:29 +0200

    feat: add info to summary

--- src/pages/basketPage/summaryInfo/SummaryInfo.tsx
+++ src/pages/basketPage/summaryInfo/SummaryInfo.tsx
@@ -1,17 +1,37 @@
 import React from 'react';
-import { Button, Card } from 'antd';
+import { Button, Card, Row } from 'antd';
 import Title from 'antd/es/typography/Title';
+import Text from 'antd/es/typography/Text';
 import styles from './SummaryInfo.module.css';
+import { useAppSelector } from '../../../hooks/hooks';
 
 function SummaryInfo() {
+  const cartItemsCount = useAppSelector(
+    (state) => state.cart.cart?.totalLineItemQuantity,
+  );
+  const totalCartPrice = useAppSelector(
+    (state) => state.cart.cart?.totalPrice.centAmount,
+  );
+  const currencyCode = useAppSelector(
+    (state) => state.cart.cart?.totalPrice.currencyCode,
+  );
+
   return (
     <Card className={styles.summaryCard}>
       <Title level={3} className={styles.title}>
         Summary
       </Title>
-      <p>content</p>
-      <p>content</p>
-      <p>Total: </p>
+      <Row>
+        <Text type="secondary" style={{ fontSize: '1em' }}>
+          {cartItemsCount}
+          {cartItemsCount && cartItemsCount > 1 ? ' items' : ' item'}
+        </Text>
+      </Row>
+      <Row>
+        <Text type="secondary" style={{ fontSize: '1em', marginBottom: '1em' }}>
+          Total: {totalCartPrice} {currencyCode}
+        </Text>
+      </Row>
       <Button type="primary">Checkout</Button>
     </Card>
   );
