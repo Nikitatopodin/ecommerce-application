230bdadb63ae8a49de70d31cc09a601c85d9b295
IlyaKhokhanov <hohanov.ilya@gmail.com>
2023-09-15 16:43:35 +0300

    feat: add buttons on cards in catalog

--- src/components/catalog/Catalog.tsx
+++ src/components/catalog/Catalog.tsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import type { MenuProps } from 'antd';
-import { Badge, Card, Drawer, Layout, List, Menu, Select } from 'antd';
+import { Badge, Button, Card, Drawer, Layout, List, Menu, Select } from 'antd';
 import { useSearchParams, useNavigate } from 'react-router-dom';
+import { LineItem } from '@commercetools/platform-sdk';
 import Sider from 'antd/es/layout/Sider';
 import Meta from 'antd/es/card/Meta';
 import { MenuOutlined } from '@ant-design/icons';
@@ -19,6 +20,8 @@ import {
 import styles from './Сatalog.module.css';
 import BreadcrumbComponent from './breadcrumb/BreadcrumbComponent';
 import PaginationCatalog from './pagination/PaginationCatalog';
+import updateCartThunk from '../../redux/actions/updateCartThunk';
+import removeCartItemThunk from '../../redux/actions/removeCartItemThunk';
 
 interface ICategory {
   label: string;
@@ -43,6 +46,7 @@ function Catalog(): JSX.Element {
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
   const { dataProducts, settings } = useAppSelector((state) => state.catalog);
+  const cart = useAppSelector((state) => state.cart.cart);
 
   const onClick: MenuProps['onClick'] = (e) => {
     dispatch(addCurrentCategory(e.key));
@@ -137,6 +141,31 @@ function Catalog(): JSX.Element {
 
   const onClose = () => {
     setOpen(false);
+  };
+
+  const getProductId = (element: LineItem, id: string) =>
+    element.productId === id;
+
+  const addItemToCart = (event: React.MouseEvent, id: string) => {
+    event.stopPropagation();
+    dispatch(updateCartThunk(cart!.version, id, 1, 1, cart!.id));
+  };
+
+  const removeItemfromCart = (
+    event: React.MouseEvent,
+    id: string,
+    price: number,
+  ) => {
+    event.stopPropagation();
+    dispatch(
+      removeCartItemThunk(
+        cart!.version,
+        cart!.id,
+        cart!.lineItems.find((elemnt) => getProductId(elemnt, String(id)))!.id,
+        'USD',
+        price,
+      ),
+    );
   };
 
   return (
@@ -270,6 +299,25 @@ function Catalog(): JSX.Element {
                     })}
                   </div>
                 </div>
+                <Button
+                  onClick={(event) =>
+                    cart?.lineItems.some((elemnt) =>
+                      getProductId(elemnt, String(item.id)),
+                    )
+                      ? removeItemfromCart(
+                          event,
+                          String(item.id),
+                          item.masterVariant.price!.value.centAmount,
+                        )
+                      : addItemToCart(event, String(item.id))
+                  }
+                >
+                  {cart?.lineItems.some((elemnt) =>
+                    getProductId(elemnt, String(item.id)),
+                  )
+                    ? 'Remove from Cart'
+                    : 'Add to Cart'}
+                </Button>
               </Card>
             </List.Item>
           )}
