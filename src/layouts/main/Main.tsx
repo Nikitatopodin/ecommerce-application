a6542e595fc6582cadd384e07780d39de268298a
Maria Bogdanova <masha.bogdanoff@gmail.com>
2023-09-19 13:43:19 +0200

    fix: main adaptive layout

--- src/layouts/main/Main.tsx
+++ src/layouts/main/Main.tsx
@@ -1,24 +1,18 @@
 import React from 'react';
-import { Layout, Typography } from 'antd';
+import { Layout } from 'antd';
 import styles from './Main.module.css';
 import HomePageCarousel from './carousel/HomePageCarousel';
 import PromoCodes from './promoCodes/promoCodes';
 
 const { Content } = Layout;
-const { Title } = Typography;
-
-const titleStyle: React.CSSProperties = {
-  fontWeight: 300,
-  fontSize: '2.3em',
-};
 
 export default function MainComponent() {
   return (
     <Content className={styles.main}>
-      <Title style={titleStyle}>
-        POSTCARDS THAT WILL INCREASE YOUR AVERAGE CHECK <br />
-        AND WILL BE REMEMBERED BY YOUR CLIENTS
-      </Title>
+      <h3 className={styles.title}>
+        POSTCARDS THAT WILL INCREASE YOUR AVERAGE CHECK AND WILL BE REMEMBERED
+        BY YOUR CLIENTS
+      </h3>
       <HomePageCarousel />
       <PromoCodes />
     </Content>
